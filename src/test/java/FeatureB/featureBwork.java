98d87656526393210ce75a90940eb73a3f53ec0d
ismailD <issmailduman@gmail.com>
2021-12-26 19:46:15 +0100

    FeatureB Started

---
+++ src/test/java/FeatureB/featureBwork.java
@@ -0,0 +1,7 @@
+package FeatureB;
+
+public class featureBwork {
+    public static void main(String[] args) {
+        System.out.println("feature B  work started");
+    }
+}
--- src/test/java/FeaturebWORK/featureBWork.java
+++
@@ -1,9 +0,0 @@
-package FeaturebWORK;
-
-import com.google.gson.internal.bind.util.ISO8601Utils;
-
-public class featureBWork {
-    public static void main(String[] args) {
-        System.out.println("Featurre B wORK");
-    }
-}
